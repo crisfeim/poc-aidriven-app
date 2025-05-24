33ea8a4a4d7ea93e050db739d6555d3ac5718d82
Cristian Rojas <cristian.rojas@live.fr>
2025-05-24 05:50:05 +0200

    Initial Commit

---
+++ AI_Counter/AI_CounterApp.swift
@@ -0,0 +1,12 @@
+// © 2025  Cristian Felipe Patiño Rojas. Created on 24/5/25.
+
+import SwiftUI
+
+@main
+struct AI_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
